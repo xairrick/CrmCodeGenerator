3e411f3b3a1f195e38b261146271bb10c9fb62a5
Eric Labashosky(d) <xairrick@gmail.com>
2014-02-15 12:54:05 -0500

    [REFACTOR] Cleaning up the code in DteHelper

--- CrmCodeGenerator.VSPackage/Helpers/DteHelper.cs
+++ CrmCodeGenerator.VSPackage/Helpers/DteHelper.cs
@@ -9,44 +9,6 @@ namespace CrmCodeGenerator.VSPackage.Helpers
 {
     public static class DteHelper
     {
-        //public static bool CheckIfFileExistsInProject(ProjectItems projectItems, string projectFile, string folder = "")
-        //{
-        //    // initial value
-        //    bool fileExists = false;
-
-        //    projectFile = projectFile.Replace(@"\", "/");
-
-        //    // iterate project items
-        //    foreach (ProjectItem projectItem in projectItems)
-        //    {
-        //        // if the name matches
-        //        if (folder + projectItem.Name == projectFile)
-        //        {
-        //            // abort this add, file already exists
-        //            fileExists = true;
-
-        //            // break out of loop
-        //            break;
-        //        }
-        //        else if ((projectItem.ProjectItems != null) && (projectItem.ProjectItems.Count > 0))
-        //        {
-        //            // check if the file exists in the project
-        //            fileExists = CheckIfFileExistsInProject(projectItem.ProjectItems,  projectFile,  folder + projectItem.Name + "/");  
-
-        //            // if the file does exist
-        //            if (fileExists)
-        //            {
-        //                // abort this add, file already exists
-        //                fileExists = true;
-
-        //                // break out of loop
-        //                break;
-        //            }
-        //        }
-        //    }
-        //    return fileExists;
-        //}
-
         public static bool HasProjectItem(this Project project, string projectFile)
         {
             var projectItem = project.GetProjectItem(projectFile);
@@ -76,43 +38,18 @@ namespace CrmCodeGenerator.VSPackage.Helpers
                 }
                 else if ((projectItem.ProjectItems != null) && (projectItem.ProjectItems.Count > 0))
                 {
-                    // check if the file exists in the project
+                    // Drilldown on folders
                     result = GetProjectItemRecursive(projectItem.ProjectItems, projectFile, folder + projectItem.Name + "/");
 
                     // if the file does exist
                     if (result != null)
                     {
-                        // break out of loop
+                        // break out of loop & Recursion
                         break;
                     }
                 }
             }
             return result;
-        }
-        private static object GetFullPath(ProjectItem projectItem)
-        {
-            if (projectItem.Document == null)
-                return null;
-
-            string fullname;
-            var myType = projectItem.Document.GetType();
-            try
-            {
-                fullname = projectItem.Document.FullName;
-            }
-            catch (Exception)
-            {
-                try
-                {
-                    fullname = System.IO.Path.Combine(projectItem.Document.Path, projectItem.Document.Name);
-                }
-                catch (Exception)
-                {
-                    return null;
-                }
-                
-            }
-            return fullname;
         }
         public static string MakeRelative(string fromAbsolutePath, string toDirectory)
         {
@@ -166,23 +103,12 @@ namespace CrmCodeGenerator.VSPackage.Helpers
             }
             return null;
         }
-        public static Project GetSelectedProject(this EnvDTE.DTE dte)
+        public static Project GetSelectedProject(this DTE dte)
         {
             Array projects = dte.ActiveSolutionProjects as Array;
             if (projects.Length > 0)
             {
                 return projects.GetValue(0) as Project;
-            }
-            return null;
-        }
-        public static Project GetProject(this EnvDTE80.DTE2 dte, string UniqueName)
-        {
-            foreach (Project p in dte.Solution.Projects)
-            {
-                if (p.UniqueName.Equals(UniqueName, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    return p;
-                }
             }
             return null;
         }
